38c23f077e006467add80cc5b7f6867d5c931ef7
Alexandr_Bartkiv <Alexandrbartkiv@gmail.com>
2020-04-04 06:49:18 +0300

    Form was aded from outer resourse(working) Telegram link

--- indedx.js
+++ indedx.js
@@ -55,20 +55,51 @@ $(document).ready(function() {
 			scrollTop: $(`header`).offset().top
 		}, 1000,"swing");
 	});
+
+	// FORM
+	window.addEventListener("DOMContentLoaded", function() {
+
+		// get the form elements defined in your form HTML above
+		
+		var form = document.getElementById("my-form");
+		var button = document.getElementById("my-form-button");
+		var status = document.getElementById("my-form-status");
 	
-
-
-	$("#form").submit(function() {
-		$.ajax({
-			type: "POST",
-			url: "mail.php",
-			data: $(this).serialize()
-		}).done(function() {
-			$(this).find("input").val("");
-			alert("Спасибо за заявку! Скоро мы с вами свяжемся.");
-			$("#form").trigger("reset");
+		// Success and Error functions for after the form is submitted
+		
+		function success() {
+		  form.reset();
+		  button.style = "display: none ";
+		  status.innerHTML = "Thanks!";
+		}
+	
+		function error() {
+		  status.innerHTML = "Oops! There was a problem.";
+		}
+	
+		// handle the form submission event
+	
+		form.addEventListener("submit", function(ev) {
+		  ev.preventDefault();
+		  var data = new FormData(form);
+		  ajax(form.method, form.action, data, success, error);
 		});
-		return false;
-	});
+	  });
+	  
+	  // helper function for sending an AJAX request
 	
+	  function ajax(method, url, data, success, error) {
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.setRequestHeader("Accept", "application/json");
+		xhr.onreadystatechange = function() {
+		  if (xhr.readyState !== XMLHttpRequest.DONE) return;
+		  if (xhr.status === 200) {
+			success(xhr.response, xhr.responseType);
+		  } else {
+			error(xhr.status, xhr.response, xhr.responseType);
+		  }
+		};
+		xhr.send(data);
+	  }
 })
