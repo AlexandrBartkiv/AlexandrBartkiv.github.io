cb90d62b1cb85a21ff1fc1821ae735c0bd3a0192
AlexandrBartkiv <alexandrbartkiv@gmail.com>
2022-05-14 00:06:09 +0300

    Work remastered

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ var target = $('#65');
 var targetPos = target.offset().top;
 var winHeight = $(window).height();
 var scrollToElem = targetPos - winHeight;
+var styles = {
+    width: $('.work').find('div').css("width"),
+    height:$('.work').find('div').css("height")
+}
 
+$('.work').find('p').css(styles)
 $(window).scroll(function(){
   var winScrollTop = $(this).scrollTop();
   if(winScrollTop > scrollToElem){
@@ -30,11 +35,49 @@ $(window).scroll(function(){
         $('.work').find('div:eq(2)').css({'background-color':'white'})
 
         console.log($('.work').find('div:first, div:last, div:eq(3), div:eq(4)'))
-
-       
     }
    }
    scr();
+
+
+   var setBlur = function(ele, radius) {
+    $(ele).find('img').css({
+       "-webkit-filter": "blur("+radius+"px)",
+        "filter": "blur("+radius+"px)",
+        
+   });
+    },
+    tweenBlur = function(ele, startRadius, endRadius) {
+        $({blurRadius: startRadius}).animate({blurRadius: endRadius}, {
+            duration: 200,
+            easing: 'swing', 
+                            
+            step: function() {
+                setBlur(ele, this.blurRadius);
+            },
+            callback: function() {
+                setBlur(ele, endRadius);
+            }
+        });
+    };
+
+   $('.work').find('div').on('mouseover',function(){
+    console.log($(this).closest('div'))
+    tweenBlur($(this).closest('div'), 0, 5);
+    $(this).find('p').animate({
+        opacity: "1",
+    }, 200) 
+   })
+   $('.work').children().on('mouseout',function(){
+    tweenBlur($(this).closest('div'), 5, 0);
+    $(this).find('p').animate({
+        opacity: "0"
+    }, 200)    
+   })
+   $('.work').children().on('mouseup',function(){
+       console.log($(this).find('a'))
+    $(this).find('img').trigger("click")
+   })
 
     $('.forma, .popupForm').submit(function(e){
         e.preventDefault(e)
